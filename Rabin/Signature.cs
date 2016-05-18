d384407172c4d693440def8b1397e8ff35bb77e0
Sammers21 <titantins@ya.ru>
2016-05-18 21:43:04 +0300

    Класс подписи

---
+++ Rabin/Signature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace RabinLib
+
+{
+    public class Signature
+    {
+        public BigInteger S { get; set; }
+        public BigInteger I { get; set; }
+
+        public Signature(BigInteger S, BigInteger I)
+        {
+            this.S = S;
+            this.I = I;
+        }
+
+
+    }
+}
